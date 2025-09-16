93958808bbe1c92c0878e83ffb056a126c6f8bb8
1krp <oscar.dalne@gmail.com>
2025-09-16 13:36:40 +0200

    Refactor copycodes function to correctly copy ASCII values and update counter

--- lab2-files/pointers.c
+++ lab2-files/pointers.c
@@ -11,16 +11,44 @@
 
 char* text1 = "This is a string.";
 char* text2 = "Yet another thing.";
-int list1[20];
+int list1[20];        // int = 4 bytes
 int list2[20];
 int counter = 0;
 
-work(){
+
+
+void copycodes(const char* a0, int* a1, int* a2){
+    int t0 = *a0;                               // lb	t0,0(a0)
+    while(t0 != 0){                             // beqz	t0,done could be changed for <=
+        *a1 = t0;                               // sw	t0,0(a1)                        
+
+        a0++;                                   // addi	a0,a0,1
+        a1++;                                   // addi	a1,a1,4 (++) for int
+
+        int t1= *a2;                            // lw	t1,0(a2)  
+        t1++;                                   // addi	t1,t1,1   "counter++"
+        *a2 = t1;                               // sw	t1,0(a2)
+
+        t0 = *a0;                               // lb	t0,0(a0)  
+    }
+}
+
+
+void work(){
+
+char* a0 = text1;
+int* a1 = list1;
+int* a2 = &counter;                     
+copycodes(a0, a1, a2);
+
+a0 = text2;
+a1 = list2;
+a2 = &counter;
+copycodes(a0, a1, a2);
 
 }
-copycodes(){
 
-}
+
 
 
 
