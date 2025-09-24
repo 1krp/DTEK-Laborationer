f143899e9402a2072d5915a82ad60cb7ef3b7f91
Oscar Dalne <oscar.dalne@gmail.com>
2025-09-24 15:52:42 +0200

    sasa

--- SharedFolderWithVM/Random implemented/Random Number test/roulettemain.c
+++ SharedFolderWithVM/Random implemented/Random Number test/roulettemain.c
@@ -9,45 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern void print(const char*);
-extern void print_dec(unsigned int);
-extern void display_string(char*);
-extern void time2string(char*,int);
-extern void tick(int*);
-extern void delay(int);
-extern int nextprime( int );
 
-volatile unsigned short *TMR1_SR = (unsigned short*) 0x04000020;
-volatile unsigned short *TMR1_CR = (unsigned short*) 0x04000024;
-volatile unsigned short *TMR1_PERLO = (unsigned short*) 0x04000028;
-volatile unsigned short *TMR1_PERHI = (unsigned short*) 0x0400002c;
-
-volatile unsigned short *TMR1_SNAPLO = (unsigned short*) 0x04000030;
-volatile unsigned short *TMR1_SNAPHI = (unsigned short*) 0x04000034;
-
-
-/* Below is the function that will be called when an interrupt is triggered. */
-void handle_interrupt(unsigned cause) 
-{
-  if (cause & 0x10){ // check if timer interrupt
-
-    *TMR1_SR = *TMR1_SR & 0xE; // clear TO bit
-  }
-}
-
-/* Add your code here for initializing interrupts. */
-void labinit(void)
-{ 
-  *(TMR1_PERLO) = (29999999/10) & 0xFFFF;
-  *(TMR1_PERHI) = (29999999/10) >> 16;
-
-  *(TMR1_CR) = 0x7; // start clock, enables ITO
-  enable_interrupts();
-}
-
-int getsnap(volatile unsigned short *SNAPLO){
-  return (int)(*SNAPLO);
-}
 
 int hash(int seed) {
     seed = ((seed >> 16) ^ seed) * 0x45d9f3bu;
@@ -56,14 +18,23 @@ int hash(int seed) {
     return seed;
 }
 
+int randomGen(int from, int to, int seed) {
+  int diff = to - from ;
+
+  int randomNum = (seed % diff)+from;
+  return randomNum;
+}
+
 /* Your code goes into main as well as any needed functions. */
 int main() {
   //labinit();
-
-  for(int i = 1; i < 30; i++){
-    printf("%d\n", hash(i));
+  int numberList[10] = {0,0,0,0,0,0,0,0,0,0};
+  for(int i = 1; i < 3000000; i++){
+    numberList[randomGen(1, 11, hash(i))-1]++;
   }
-
+  for(int i = 0; i < 10; i++){
+    printf("Number %d was generated %d times\n", i+1, numberList[i]);
+  }
 }
 
 
